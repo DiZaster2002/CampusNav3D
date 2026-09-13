e675bf2187fa99778439e53a8b0059bfe2153e7a
DiZaster2002 <carlosdurgon2002@gmail.com>
2026-09-13 16:43:35 +0200

    refactor: modularized Celery tasks in tasks.py

--- backend/maps/services/__init__.py
+++ backend/maps/services/__init__.py
@@ -1,4 +1,5 @@
 from .plan_service import PlanService
 from .approval_service import ApprovalService
+from .ingestion_service import IngestionService
 
-__all__ = ['PlanService', 'ApprovalService']
+__all__ = ['PlanService', 'ApprovalService', 'IngestionService']
